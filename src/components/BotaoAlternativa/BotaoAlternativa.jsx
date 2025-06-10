2e4e1f4c0d034aa737c24214190f06be50a7928e
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-06-10 12:43:08 -0300

    feat: criando o componente do botão de alternativa

---
+++ src/components/BotaoAlternativa/BotaoAlternativa.jsx
@@ -0,0 +1,7 @@
+import "./BotaoAlternativa.css";
+
+const BotaoAlternativa = ({ alternativa }) => {
+  return <div className="botao-alternativa-container">{alternativa}</div>;
+};
+
+export default BotaoAlternativa;
