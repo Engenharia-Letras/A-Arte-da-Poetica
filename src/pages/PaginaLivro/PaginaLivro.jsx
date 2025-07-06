edd193a74b6e11370d8c9cf4682fdc5443de3cc1
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-07-05 23:13:06 -0300

    feat: implementando página de apresentação do texto

---
+++ src/pages/PaginaLivro/PaginaLivro.jsx
@@ -0,0 +1,37 @@
+import "./PaginaLivro.css";
+import imagemLivro from "../../assets/livro_aberto.png";
+import Sair from "../../assets/sair.png";
+import Seta from "../../assets/seta.png";
+
+const PaginaLivro = ({ texto, onFechar, onProximo }) => {
+  return (
+    <div className="pagina-livro-fundo">
+      <img
+        src={Sair}
+        alt="Ícone para Sair"
+        className="icone-sair"
+        onClick={onFechar}
+      />
+
+      <div className="livro-container">
+        <img
+          src={imagemLivro}
+          alt="Ilustração de um livro aberto"
+          className="livro-imagem"
+        />
+        <div className="livro-texto">
+          <p>{texto}</p>
+        </div>
+      </div>
+
+      <img
+        src={Seta}
+        alt="Ícone para Avançar"
+        className="icone-avancar"
+        onClick={onProximo}
+      />
+    </div>
+  );
+};
+
+export default PaginaLivro;
